7587b56751283c86b4ef68d18e48e7e0f569a749
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2012-08-01 13:44:25 +0700

    Initial commit

---
+++ lib/Task/Data/Sah.pm
@@ -0,0 +1,18 @@
+package Task::Data::Sah;
+
+# VERSION
+
+1;
+# ABSTRACT: Install all Data::Sah modules
+
+=head1 DESCRIPTION
+
+This task will install all Data::Sah modules.
+
+=pkgroup Included modules
+
+=pkg Data::Sah
+
+=pkg Sah
+
+=cut
